fd3849d346e5089d760f73d5e01c0e2f485f4273
Karol Ponikowski <k.ponikowski.98@gmail.com>
2023-10-31 12:23:32 +0100

    Start project + Add Dio

--- lib/domain/repositories/authors_repository.dart
+++ lib/domain/repositories/authors_repository.dart
@@ -1,10 +1,11 @@
 import 'package:user_articles/data/remote_data_sources/authors_remote_data_source.dart';
 import 'package:user_articles/domain/models/author_model.dart';
 
+
 class AuthorsRepository {
   AuthorsRepository({required this.remoteDataSource});
 
-  final AuthorsMockedDataSource remoteDataSource;
+  final AuthorsRemoteDioDataSource remoteDataSource;
 
   Future<List<AuthorModel>> getAuthorModels() async {
     final json = await remoteDataSource.getAuthors();
